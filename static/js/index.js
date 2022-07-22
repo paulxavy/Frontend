5473154d717acf5467a003e51e16e7f1107ea971
paulxavy <95659668+paulxavy@users.noreply.github.com>
2022-07-21 20:16:35 -0500

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,29 @@
+let endPoint = 'https://flask-backenduce.herokuapp.com/upload';
+// Get textarea - Dom
+function getVal() {
+    console.log("si va")
+    const val = document.querySelector('input').value;
+    let data = {data: val};
+    fetch(endPoint, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res =>  res.json())
+    .then(response => {
+        console.log('Success:', response)
+        console.log(response.value);
+        let result = document.getElementById("result");
+        result.value = response.value;
+    })
+    .catch(error => console.error(error))
+    
+
+   
+}
+function limpiar() {
+    document.getElementById("text").value = "";
+    document.getElementById("result").value = "";
+}
+
